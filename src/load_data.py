32ae2bb497c419df04cade8e741631f5844fc89b
skycoder-akash <9696akashverma@gmail.com>
2026-06-29 23:36:58 +0530

    Added Module 2: Data Inspection

--- src/load_data.py
+++ src/load_data.py
@@ -24,3 +24,29 @@ print(df.columns.tolist())
 
 print("\n5. Data Types")
 print(df.dtypes)
+
+print("\n" + "=" * 60)
+print("MODULE 2 : DATA INSPECTION")
+print("=" * 60)
+
+
+print("\n1. Missing Values")
+print(df.isnull().sum())
+
+print("\n2. Duplicate Records")
+print("Duplicate Rows :", df.duplicated().sum())
+
+# Descriptive Statistics
+print("\n3. Descriptive Statistics")
+print(df.describe())
+
+# Memory Usage
+print("\n4. Memory Usage")
+print(df.memory_usage())
+
+print("\nTotal Memory Used:")
+print(df.memory_usage(deep=True).sum(), "bytes")
+
+# Summary Information
+print("\n5. Dataset Information")
+df.info()
