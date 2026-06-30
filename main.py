2ef53071adb0b7c59deb4fd2730295a6119c3e4a
skycoder-akash <9696akashverma@gmail.com>
2026-06-30 23:50:49 +0530

    Remove unnecessary lines of code

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import os
 
-print("=" * 60)
-print("STUDENT PERFORMANCE DATA HANDLING SYSTEM")
-print("=" * 60)
-
 os.system("python src/load_data.py")
 os.system("python src/clean_data.py")
 os.system("python src/transform.py")
